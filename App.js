39b115564b27256c1c25dd468d14f2ee986ea839
vuthihathu <vuthihathu2000@gmail.com>
2021-11-05 20:26:28 +0700

    add

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+
+import CartScreen from './src/views/screens/CartScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StackNavigation } from './StackNavigation';
+import { Ionicons } from '@expo/vector-icons';
+import HomeScreen from './src/views/screens/HomeScreen';
+import TabNavigation from './TabNavigation';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <TabNavigation/>
+  </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
