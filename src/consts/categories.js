cb8d0304bfce6d60c2d49d0ea9a24739f31b103b
vuthihathu <vuthihathu2000@gmail.com>
2021-11-05 20:17:31 +0700

    add

---
+++ src/consts/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+    {id: '1', name: 'dress', image: require('../assets/dress.png')},
+    {id: '2', name: 'shoes', image: require('../assets/shoes.png')},
+    {id: '3', name: 't-shirt', image: require('../assets/t-shirt.png')},
+    {id: '4', name: 'trousers', image: require('../assets/trousers.png')},
+  ];
+  
+  export default categories;
