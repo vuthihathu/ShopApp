07f264775e400ec35398cd2f130f98ffef9738ba
vuthihathu <vuthihathu2000@gmail.com>
2021-11-05 20:18:56 +0700

    add

---
+++ src/consts/dresses.js
@@ -0,0 +1,33 @@
+const dresses = [
+    {
+      id: '1',
+      name: 'Váy xòe',
+      ingredients: 'Xòe Xòe',
+      price: '100000',
+      image: require('../assets/dress1.png'),
+    },
+    {
+      id: '2',
+      name: 'Váy đỏ',
+      ingredients: 'Quyến rũ',
+      price: '200000',
+      image: require('../assets/red.png'),
+    },
+    {
+      id: '3',
+      name: 'Váy tím ',
+      ingredients: 'Tím thủy chung nè ',
+      price: '300000',
+      image: require('../assets/dress1.png'),
+    },
+    {
+      id: '4',
+      name: 'Váy vàng',
+      ingredients: 'Đẹp như màu nắng',
+      price: '400000',
+      image: require('../assets/dress1.png'),
+    },
+  ];
+  
+  export default dresses;
+  
